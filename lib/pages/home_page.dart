0fe4728b0a6e73f5d4efe69de926db0a8bf6a0a7
prikshitmaster <106397426+prikshitmaster@users.noreply.github.com>
2022-06-21 18:09:07 +0530

    sliderview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
-import 'package:carousel_slider/carousel_slider.dart';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
 import '../drawer/drawerSlider.dart';
 
@@ -10,26 +13,63 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black, size: 30),
+        iconTheme: const IconThemeData(color: Colors.black, size: 30),
         backgroundColor: Colors.white,
       ),
-      body: CarouselSlider(
-        options: CarouselOptions(height: 400.0),
-        items: _image.map((i) {
-          return Builder(
-            builder: (BuildContext context) {
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                child: Image.asset(i),
-              );
-            },
-          );
-        }).toList(),
+      body: Column(
+        children: [
+          ImageSlideshow(
+            width: double.infinity,
+            height: 500,
+            initialPage: 0,
+            indicatorColor: Colors.grey,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: Image.asset(
+                  'assets/graphic.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Image.asset(
+                'assets/graphic.png',
+                fit: BoxFit.cover,
+              ),
+              Image.asset(
+                'assets/seo.jpg',
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Card(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxHeight: 105),
+                  child: Image.network('https://bit.ly/39Gtfpu'),
+                ),
+              ),
+              Card(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxHeight: 105),
+                  child: Image.network('https://bit.ly/3HIl6gE'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
       drawer: DrawerView(),
     );
   }
 
-  final List _image = ['assets/appdev.jpg', ' assets/seo.jpg', 'webdev.jpg'];
+  final List _image = [
+    "https://bit.ly/3QuwSiZ",
+    " https://bit.ly/3QDrPfU",
+    "https://bit.ly/3QuwSiZ",
+    " https://bit.ly/3QDrPfU"
+  ];
 }
