a2dccb6ef1aec49b97859bbdfa076402d3686388
prikshitmaster <prikshitcorp@gmail.com>
2022-06-29 16:32:55 +0530

    testimonail half changes

--- lib/pages/Career_Page.dart
+++ lib/pages/Career_Page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Career extends StatelessWidget {
   const Career({Key? key}) : super(key: key);
@@ -18,9 +20,20 @@ class Career extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       body: Center(
+        child: Link(
+            uri: Uri.parse('https://webworlddeveloping.com/pf.php'),
+            builder: (
+              context,
+              followlink,
+            ) {
+              return GestureDetector(
+                onTap: followlink,
+                child: Text('CLick here'),
+              );
+            }),
 
-        child: Lottie.network(
-            'https://assets2.lottiefiles.com/packages/lf20_kcsr6fcp.json'),
+        //   child: Lottie.network(
+        //       'https://assets2.lottiefiles.com/packages/lf20_kcsr6fcp.json'),
       ),
 
     );
--- lib/pages/Contact_Page.dart
+++ lib/pages/Contact_Page.dart
@@ -1,7 +1,6 @@
 import 'package:url_launcher/link.dart';
 import 'package:flutter/material.dart';
 
-
 class Contact extends StatelessWidget {
   const Contact({Key? key}) : super(key: key);
 
@@ -9,9 +8,9 @@ class Contact extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          iconTheme: IconThemeData(color: Colors.black, size: 30),
+          iconTheme: const IconThemeData(color: Colors.black, size: 30),
           backgroundColor: Colors.white,
-          title: Text(
+          title: const Text(
             "Contact Us",
             style: TextStyle(color: Colors.black),
           ),
@@ -20,36 +19,37 @@ class Contact extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 15,
               ),
               Align(
-                alignment: Alignment.centerLeft + Alignment(0.5, 1),
-                child: Text(
+                alignment: Alignment.centerLeft + const Alignment(0.5, 1),
+                child: const Text(
                   "Get in Touch",
                   style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25),
                 ),
               ),
-              Divider(
+              const Divider(
                 height: 30,
                 thickness: 2,
                 indent: 20,
                 endIndent: 150,
               ),
               Container(
-                padding: EdgeInsets.only(left: 25) + EdgeInsets.only(right: 25),
-                child: Align(
+                padding: const EdgeInsets.only(left: 25) +
+                    const EdgeInsets.only(right: 25),
+                child: const Align(
                   child: Text(
                     "Hi - we're happy to hear from you! Use our contacts below to find more information about us. Thank you for your interest in Web World Developing.",
                     style: TextStyle(fontWeight: FontWeight.w300, fontSize: 15),
                   ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 15,
               ),
               Container(
-                padding: EdgeInsets.only(top: 20),
+                padding: const EdgeInsets.only(top: 20),
                 height: 350,
                 width: 500,
                 child: Card(
@@ -58,65 +58,58 @@ class Contact extends StatelessWidget {
                     children: [
                       Column(
                         children: [
-                          Container(
-                              child: Padding(
-                                padding: EdgeInsets.all(10),
-                                child: ListTile(
-                                  leading: Image(
-                                      image: NetworkImage(
-                                          'https://webworlddeveloping.com/img/contact7.png')),
-                                  title: Link(
-                                    target: LinkTarget.blank,
-                                    uri: Uri.parse(
-                                        'https://webworlddeveloping.com/contacts.php'),
-                                    builder: (context, followlink) =>
-                                        GestureDetector(
-                                          onTap: followlink,
-
-                                          child: Text('Our Office',
-                                            style: TextStyle(
-                                                color: Colors.blue
-                                            ),),
-
-                                        ),
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: ListTile(
+                              leading: const Image(
+                                  image: NetworkImage(
+                                      'https://webworlddeveloping.com/img/contact7.png')),
+                              title: Link(
+                                target: LinkTarget.blank,
+                                uri: Uri.parse(
+                                    'https://webworlddeveloping.com/contacts.php'),
+                                builder: (context, followlink) =>
+                                    GestureDetector(
+                                  onTap: followlink,
+                                  child: const Text(
+                                    'Our Office',
+                                    style: TextStyle(color: Colors.blue),
                                   ),
-                                  subtitle: Text(
-                                      "Office No. 204/205, Rajhans City, Opp. Kirti Petroleum, Nr. Lalpur Bypass Chowkdi, Ranjit Sagar Rd, Jamnagar, Gujarat 361005. "),
                                 ),
-                              )),
-                          Container(
-                              child: Padding(
-                                padding: EdgeInsets.all(10),
-                                child: ListTile(
-                                  leading: Image(
-                                      image: NetworkImage(
-                                          'https://webworlddeveloping.com/img/contact8.png')),
-                                  title: Text('Online Support 24x7'),
-                                  subtitle: Text("Click here Get Support "),
-                                ),
-                              )),
-                          Container(
-                              child: Padding(
-                                padding: EdgeInsets.all(10),
-                                child: ListTile(
-                                  leading: Image(
-                                      image: NetworkImage(
-                                          'https://webworlddeveloping.com/img/contact9.png')),
-                                  title: Text('Call Us '),
-                                  subtitle: Text("+91 9033764484 "),
-                                ),
-                              )),
+                              ),
+                              subtitle: const Text(
+                                  "Office No. 204/205, Rajhans City, Opp. Kirti Petroleum, Nr. Lalpur Bypass Chowkdi, Ranjit Sagar Rd, Jamnagar, Gujarat 361005. "),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(10),
+                            child: ListTile(
+                              leading: Image(
+                                  image: NetworkImage(
+                                      'https://webworlddeveloping.com/img/contact8.png')),
+                              title: Text('Online Support 24x7'),
+                              subtitle: Text("Click here Get Support "),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(10),
+                            child: ListTile(
+                              leading: Image(
+                                  image: NetworkImage(
+                                      'https://webworlddeveloping.com/img/contact9.png')),
+                              title: Text('Call Us '),
+                              subtitle: Text("+91 9033764484 "),
+                            ),
+                          ),
                         ],
                       ),
                     ],
                   ),
                 ),
               ),
-              Container(
-
-                child: Image(
-                  image: AssetImage('assets/map.jpg'), fit: BoxFit.fill,
-                ),
+              const Image(
+                image: AssetImage('assets/map.jpg'),
+                fit: BoxFit.fill,
               ),
             ],
           ),
